1599b44d0adf4ab3f632884fe0ca3b2431e28927
Yidan Wang <wangyidan621@gmail.com>
2017-10-31 21:07:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  kittens.slice(0,kittens.length-1)
+return  kittens.slice(0,kittens.length-1)
 }
 
 function removeFirstKitten(){
-  kittens.slice(1)
+return  kittens.slice(1)
 }
